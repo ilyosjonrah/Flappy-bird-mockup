06bbb95a1da4d3683a35a7dcf587ba580bff385a
ilyosjonrah <ilyosjonrah@gmail.com>
2024-09-06 01:25:08 +0700

    #

--- script - Copy.js
+++ script - Copy.js
@@ -1,8 +1,10 @@
 
+
+// Get canvas
 let canvas = document.getElementById('canvas')
 let context = canvas.getContext('2d')
 
-
+// Load images
 const background = new Image()
 let bird = new Image()
 const floor = new Image()
@@ -12,14 +14,14 @@ const scoreboard = new Image()
 const gameoverBoard = new Image()
 const messageReady = new Image()
 
-
+// Load Audio
 const SFX_die = new Audio()
 const SFX_hit = new Audio()
 const SFX_point = new Audio()
 const SFX_swoosh = new Audio()
 const SFX_wing = new Audio()
 
-
+// Bird Frame array for animation
 const birdImages = [
     './images/bird1.png',
     './images/bird2.png',
@@ -27,9 +29,9 @@ const birdImages = [
     './images/bird2.png',
 ]
 
-
+// images
 background.src = "./images/bg.png"
-
+//bird.src = "./images/bird.png"
 upperPipe.src = "./images/toppipe.png"
 lowerPipe.src = "./images/bottompipe.png"
 floor.src = "./images/fg.png"
@@ -37,7 +39,7 @@ scoreboard.src = "./images/scoreboard.png"
 gameoverBoard.src = './images/gameOver.png'
 messageReady.src = "./images/message.png"
 
-
+//audio
 SFX_die.src = "./assets/sfx/die.wav"
 SFX_hit.src = "./assets/sfx/hit.wav"
 SFX_point.src = "./assets/sfx/point.wav"
@@ -45,7 +47,7 @@ SFX_swoosh.src = "./assets/sfx/swoosh.wav"
 SFX_wing.src = "./assets/sfx/wing.wav"
 
 
-
+// Variables for dimensions, etc
 bird.src = birdImages[0]
 let birdDimensionX = 40
 let birdDimensionY = 30
@@ -62,7 +64,7 @@ pipe[0] = {
     y: 0
 }
 
-
+// Variables for settings
 const gap = 130
 const maxJump = 30
 let velocity = 1
@@ -73,21 +75,28 @@ const hole = upperPipe.height + gap
 let score = 0
 let isKeyPressed = false
 
+// Load Fonts
+const loadFont = new FontFace('FlappybirdFont', 'url(./assets/flappy-bird-font.TTF)')
+loadFont.load().then(function (font) {
+    document.fonts.add(font)
+}).catch(function (error) {
+    console.error('Font loading failed:', error);
+})
+
+// Code taken somewhere from the internet
+    // const rotateImage = (context, img, x, y, width, height, angle) => {
+    //     let translateX = x + birdDimensionX / 2
+    //     let translateY = y + birdDimensionY / 2
+    //     context.save()
+    //     context.translate(translateX, translateY)
+    //     context.rotate(angle)
+    //     context.translate(-translateX, -translateY)
+    //     context.drawImage(img, x, y, width, height)
+    //     context.restore()
+    // }
 
 
-const rotateImage = (context, img, x, y, width, height, angle) => {
-    let translateX = x + birdDimensionX / 2
-    let translateY = y + birdDimensionY / 2
-    context.save()
-    context.translate(translateX, translateY)
-    context.rotate(angle)
-    context.translate(-translateX, -translateY)
-    context.drawImage(img, x, y, width, height)
-    context.restore()
-}
-
-
-let gameState = 2 
+let gameState = 2 // 0 Over, 1 Play, 2 Idle
 const eventFunction = () => {
     isKeyPressed = true
     jumpPerFrame = 5
@@ -115,7 +124,7 @@ const eventFunction = () => {
 
 const checkKey = (e) => {
     if (e.keyCode == 38 || e.keyCode == 32) {
-        e.preventDefault() 
+        e.preventDefault() // Prevent scrolldown for 'space'
         eventFunction()
     }
 }
@@ -126,8 +135,8 @@ document.addEventListener('touchstart', eventFunction)
 
 let jumpAccel = 0
 let rotate = 0
-let frames = 0  
-let birdJumpFrame = 0   
+let frames = 0  // Frames for bird animation wing flap
+let birdJumpFrame = 0   // How many frames after input for bird to rotate down when falling
 let birdIndex = 0
 let idleInterval
 let floorPosition = 0
@@ -141,7 +150,7 @@ window.onload = draw = () => {
     context.drawImage(background, bgPosition, 0, canvas.width, canvas.height)
     context.drawImage(background, bgPosition + canvas.width, 0, canvas.width, canvas.height)
 
-  
+    // Change bird image every 50 frames
     if (frames % 50 == 0) {
         birdIndex += 1
         if (birdIndex === birdImages.length) {
@@ -156,13 +165,13 @@ window.onload = draw = () => {
     } else if (gameState === 2) {
         idleState()
     }
- 
+    // Move and draw the floor
     floorPosition -= velocity
     if (floorPosition <= -canvas.width) {
         floorPosition = 0
     }
 
-
+    // Draw the floor multiple times for looping effect
     context.drawImage(floor, floorPosition, canvas.height - floor.height, canvas.width, floor.height)
     context.drawImage(floor, floorPosition + canvas.width, canvas.height - floor.height, canvas.width, floor.height)
 
@@ -180,10 +189,10 @@ let idleFly = 0
 let idleFlyDirection = 'up'
 const idleState = () => {
 
-  
+    // Get Ready
     context.drawImage(messageReady, 145, 140, messageReady.width * 1.1, messageReady.height * 1.1)
 
- 
+    // For every <value> frames, change bird image for animation
     if (frames % 65 == 0) {
         birdIndex += 1
         if (birdIndex === birdImages.length) {
@@ -192,6 +201,8 @@ const idleState = () => {
     }
     frames++
 
+    // Idle animation
+    // Bird moves up and down in the context
     if (idleFly === 20) {
         idleFlyDirection = 'down';
     }
@@ -205,52 +216,53 @@ const idleState = () => {
         bY -= 0.5
         idleFly = idleFly - 0.5
     }
- 
+    // Draw Bird with new image source
     bird.src = birdImages[birdIndex]
     rotate = 0
     rotateImage(context, bird, bX, bY, birdDimensionX, birdDimensionY, rotate);
 }
 
-const playGame = () => {
-    if (!isKeyPressed) {
-  
-        jumpPerFrame += gravity
-        bY += jumpPerFrame
-        if (birdJumpFrame <= 39 && birdJumpFrame != 0) {
-            rotate = -35 * Math.PI / 180
-        }
- 
-        else if (birdJumpFrame >= 40) {
-            rotate = 75 * Math.PI / 180
-        }
-        birdJumpFrame++
-        bird.src = birdImages[birdIndex]
-        rotateImage(context, bird, bX, bY, birdDimensionX, birdDimensionY, rotate);
-    }
- 
-    else {
-  
-        bY -= jumpPerFrame
-        jumpPerFrame -= .17
-        rotate = -35 * Math.PI / 180
-        bird.src = birdImages[birdIndex]
-        SFX_wing.play()
-        rotateImage(context, bird, bX, bY, birdDimensionX, birdDimensionY, rotate);
+// const playGame = () => {
+//     if (!isKeyPressed) {
+//         // As it falls down, speed accelerates
+//         jumpPerFrame += gravity
+//         bY += jumpPerFrame
+//         // Have it still face upwards until it accelerates down
+//         if (birdJumpFrame <= 39 && birdJumpFrame != 0) {
+//             rotate = -35 * Math.PI / 180
+//         }
+//         // As it falls down rotate bid to face downwards
+//         else if (birdJumpFrame >= 40) {
+//             rotate = 75 * Math.PI / 180
+//         }
+//         birdJumpFrame++
+//         bird.src = birdImages[birdIndex]
+//         rotateImage(context, bird, bX, bY, birdDimensionX, birdDimensionY, rotate);
+//     }
+//     // Move up
+//     else {
+//         // As it jump, have a higher acceleration and then decelerate until no more jump
+//         bY -= jumpPerFrame
+//         jumpPerFrame -= .17
+//         rotate = -35 * Math.PI / 180
+//         bird.src = birdImages[birdIndex]
+//         SFX_wing.play()
+//         rotateImage(context, bird, bX, bY, birdDimensionX, birdDimensionY, rotate);
+//         // If no more jump, reset values
+//         if (jumpPerFrame <= 0) {
+//             isKeyPressed = false
+//             jumpPerFrame = 0
+//             rotate = 0
+//             birdJumpFrame = 1
+//         }
+//     }
 
-        if (jumpPerFrame <= 0) {
-            isKeyPressed = false
-            jumpPerFrame = 0
-            rotate = 0
-            birdJumpFrame = 1
-        }
-    }
-
-    createPipes()
-}
+//     createPipes()
+// }
 
 
 const createPipes = () => {
- 
+    // Generate pipes
     for (var i = 0; i < pipe.length; i++) {
 
         constant = pipeDimensionY + gap;
@@ -259,7 +271,7 @@ const createPipes = () => {
 
         pipe[i].x -= velocity;
 
-
+        // Make new pipe
         if (pipe[i].x == Math.floor(canvas.width - 250)) {
             pipe.push({
                 x: canvas.width,
@@ -267,21 +279,22 @@ const createPipes = () => {
             });
         }
 
-   
+        // Remove pipes
         if (pipe[i].x === 0 - pipeDimensionX) {
             pipe.shift()
         }
 
-
-        if (bX + bird.width >= pipe[i].x &&     
-            bX <= pipe[i].x + pipeDimensionX &&  
-            (bY <= pipe[i].y + pipeDimensionY || 
-                bY + bird.height >= pipe[i].y + constant) 
-            ) {
+        // Collision
+        // CodeExplainedRepo Github
+        if (bX + bird.width >= pipe[i].x &&     // If bird X position and width is greater or equal to pipe X dimensions
+            bX <= pipe[i].x + pipeDimensionX &&  // if bird X position is lesser than pipe X position + height
+            (bY <= pipe[i].y + pipeDimensionY || // If bird is lower than pipe Y and its height
+                bY + bird.height >= pipe[i].y + constant) /* || // OR if bird is greater than pipe + constnat
+            bY + bird.height >= canvas.height - floor.height */) {
 
             SFX_hit.play()
             gameState = 0
-        } 
+        } // If it falls to the floor
         else if (bY + bird.height >= canvas.height - floor.height) {
             SFX_die.play()
             gameState = 0
@@ -291,6 +304,7 @@ const createPipes = () => {
             SFX_point.play()
         }
 
+        //score
         context.font = "45px FlappybirdFont";
         context.strokeStyle = "black";
         context.fillStyle = 'white'
@@ -305,4 +319,61 @@ const createPipes = () => {
         }
 
     }
+}
+
+// const updateHighscore = (score) => {
+//     let currentHighscore = localStorage.getItem('highscore');
+
+//     if (currentHighscore === 'undefined') {
+//         // If no highscore, create
+//         localStorage.setItem('highscore', JSON.stringify(score));
+//         return score
+//     }
+
+//     // Parse score
+//     currentHighscore = JSON.parse(currentHighscore);
+
+//     // If score is higher than highscore, update highscore
+//     if (currentHighscore <= score) {
+//         localStorage.setItem('highscore', JSON.stringify(score))
+//         return score
+//     }
+//     else {
+//         return currentHighscore
+//     }
+
+// }
+
+const displayScoreboard = () => {
+    disableInput()
+    context.drawImage(scoreboard, canvas.width / 4, canvas.height / 3, scoreboard.width, scoreboard.height)
+    context.font = "28px FlappybirdFont";
+    context.strokeStyle = "black";
+    context.fillStyle = 'white'
+    context.lineWidth = '1'
+
+    //GameOver
+    context.drawImage(gameoverBoard, (canvas.width / 2) / 2, 150, gameoverBoard.width * 1.2, gameoverBoard.height * 1.2)
+
+    // Current Score
+    context.fillText(score, canvas.width / 2 + 75, canvas.height / 2 - 50)
+    context.strokeText(score, canvas.width / 2 + 75, canvas.height / 2 - 50)
+
+    // High score
+    const highscore = updateHighscore(score)
+    context.fillText(highscore, canvas.width / 2 + 75, canvas.height / 2)
+    context.strokeText(highscore, canvas.width / 2 + 75, canvas.height / 2)
+}
+
+
+// Disable inputs for x seconds
+const disableInput = () => {
+    document.removeEventListener('touchstart', eventFunction)
+    document.removeEventListener('keydown', checkKey)
+    document.removeEventListener('click', eventFunction)
+    setTimeout(() => {
+        document.addEventListener('touchstart', eventFunction)
+        document.addEventListener('keydown', checkKey)
+        document.addEventListener('click', eventFunction)
+    }, 500);
 }
